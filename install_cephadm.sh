23bf43f07783f94dcaf7b349cd3c18b375623492
Trần Phúc Hậu <hautp@gocit.vn>
2022-08-16 15:33:49 +0700

    Create install_cephadm.sh

---
+++ install_cephadm.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+############################################################
+# CephAdmin Install Script for Debian 10 (Bullseye)
+# 
+# run as sudo 
+############################################################
+
+# determine if we run as sudo
+userid="${SUDO_USER:-$USER}"
+if [ "$userid" == 'root' ]
+  then 
+    echo "Please run the setup as sudo and not as root!"
+    exit 1
+fi
+if [ "$EUID" -ne 0 ]
+  then 
+    echo "Please run setup as sudo!"
+    exit 1
+fi
+
+echo "###############################################"
+echo " installing cephadm on deployment-host only...."
+echo "###############################################"
+curl --silent --remote-name --location https://github.com/ceph/ceph/raw/pacific/src/cephadm/cephadm
+mv cephadm /usr/local/bin
+chmod +x /usr/local/bin/cephadm
+mkdir -p /etc/ceph
+
+# add ceph common tools
+cephadm add-repo --release pacific
+cephadm install ceph-common
